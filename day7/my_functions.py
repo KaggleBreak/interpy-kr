71f1536d7a8eca4787e0e30c186c693b8741cab4
WonTaek <time_to_bye@naver.com>
2019-07-06 09:07:10 +0900

    upload oneliners_and_underscore file

---
+++ day7/my_functions.py
@@ -0,0 +1,7 @@
+## filename:- my_functions.py
+
+def func():
+    return "datacamp"
+
+def _private_func():
+    return 7
---
+++ day7/my_script.py
@@ -0,0 +1,5 @@
+def f(x):
+    for _ in range(1000):
+        x += 1
+    return x
+f(12)
